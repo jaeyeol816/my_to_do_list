ec76ac47ddc995783b08f490c89f4955a05cccd8
JaeyeolChoi <jaeyeolchoi@gmail.com>
2021-05-14 23:52:48 +0900

    add checkbox on list

--- execute.js
+++ execute.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let summitButton = document.getElementById("summitButton");
-//summitButton.addEventListener("click", addList);  //summitButton 클릭시 addList함수 실행  
+//summitButton.addEventListener("click", addList);  //summitButton 클릭시 addList함수 실행(대신onclick함.)
 
 function addList() {
   let newItem = document.createElement("li");   //새로운 li요소 생성
@@ -9,19 +9,17 @@ function addList() {
   let appendingData = document.querySelector("#appendingData");   //폼의 텍스트 필드 가져오기
   let newText = document.createTextNode(appendingData.value); //가져온 텍스트 필드의 값을 새 텍스트 노드로 만든다.
 
-  /*
+  
   let newCheckBox = document.createElement("input");  //체크박스로 쓸 input요소 생성
-  let newCheckBoxAttribute = document.createAttribute("type");
-  newCheckBoxAttribute.value="checkbox"
-  newCheckBox.setAttribute(newCheckBoxAttribute);
+  newCheckBox.setAttribute("type", "checkbox");
 
-  console.log("ok"); //test code
-  */
+
+  
   let newDeleteButton = document.createElement("button"); //삭제버튼으로 쓸 button요소 생성
   let newButtonText = document.createTextNode("X"); //만든 button의 텍스트노드 생성
   newDeleteButton.appendChild(newButtonText); //button요소의 자식으로 텍스트노드를 연결
 
-  //newItem.appendChild(newCheckBox);
+  newItem.appendChild(newCheckBox);
   newItem.appendChild(newText);
   newItem.appendChild(newDeleteButton);
 
@@ -35,7 +33,7 @@ function addList() {
   //여기부터 리스트 삭제 관련 코드 (왜 이것이 addList함수 안에 있어야 될까?)
   
   let itemsButtons = document.querySelectorAll("li > button");
-  console.log("ok");  //testcode
+
 
   for (let i = 0; i < itemsButtons.length; i++) {   //각각의 X버튼에 이벤트리스너 달기
     itemsButtons[i].addEventListener("click", delNode);
